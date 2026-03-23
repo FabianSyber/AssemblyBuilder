074554e1137c963a0346ab5beb6cc01bd3236f09
FabianSyber <fabian.sellberg@hotmail.com>
2026-03-23 08:43:17 +0100

    feat: add UI barrel export

---
+++ src/ui/index.ts
@@ -0,0 +1,4 @@
+// Common
+export { default as GlassCard } from './common/GlassCard.vue'
+export { default as BaseButton } from './common/BaseButton.vue'
+export { default as Badge } from './common/Badge.vue'
